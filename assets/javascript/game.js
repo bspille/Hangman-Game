7bdee7a02c4778ce6f9a11cf0f56bdc6779eed57
Benjamin Spille <Benjamin Spille>
2017-01-30 11:50:41 -0500

    Added a word bank and started putting together a regular expression

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,16 +7,26 @@ document.getElementById("dialog").innerHTML = "What is this?";
 
 document.getElementById("counter").innerHTML = 0;
 
-document.getElementById("word").innerHTML = "_ ";
+// document.getElementById("word").innerHTML = "_ ";
 
 document.getElementById("life").innerHTML = 13;
 
+// word bank used for the game
+var wordBank = ["strike", "freedom",
+"justice", "dominion"]
 
+// selects word from word bank at random
+var ranWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+document.getElementById("word").innerHTML = ranWord;
 
+// provides keyboard input for the game
 document.onkeyup = function(event) {
  	var guess = event.key;
 	var guessed = document.getElementById("log").innerHTML + guess;
 	var tries = document.getElementById("life").innerHTML - 1;
+	var hidWord = document.getElementById("word").innerHTML;
+	var char = hidWord.search(/guess/g)
+	document.getElementById("dialog").innerHTML = char;
 	document.getElementById("log").innerHTML = guessed;	
 	document.getElementById("life").innerHTML = tries;
 }
