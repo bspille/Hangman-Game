c70d216960721e193e448b3c27d12984a4750f92
Benjamin Spille <Benjamin Spille>
2017-01-29 22:07:32 -0500

    completed count down for number of guesses remaining

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,12 +11,13 @@ document.getElementById("word").innerHTML = "_ ";
 
 document.getElementById("life").innerHTML = 13;
 
-// document.getElementById("log").innerHTML = "A B C";
 
 
 document.onkeyup = function(event) {
  	var guess = event.key;
 	var guessed = document.getElementById("log").innerHTML + guess;
-	document.getElementById("log").innerHTML = guessed;
+	var tries = document.getElementById("life").innerHTML - 1;
+	document.getElementById("log").innerHTML = guessed;	
+	document.getElementById("life").innerHTML = tries;
 }
 
