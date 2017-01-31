c19ea7024bb113e2a9b210d7927e4d632e5530e1
Benjamin Spille <Benjamin Spille>
2017-01-31 11:10:49 -0500

    Added for loop and if statement that returns indices of the event key to a array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,19 +15,39 @@ document.getElementById("life").innerHTML = 13;
 var wordBank = ["strike", "freedom",
 "justice", "dominion"]
 
+
+
+
+
 // selects word from word bank at random
 var ranWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 document.getElementById("word").innerHTML = ranWord;
 
 // provides keyboard input for the game
 document.onkeyup = function(event) {
+	// the key that is played
  	var guess = event.key;
+ 	// index value of the key played
+	var indices = [];
+	// the target word generated from a word bank array
+	var target = document.getElementById("word").innerHTML;
+ 	 // the keys that where played
 	var guessed = document.getElementById("log").innerHTML + guess;
+	// the count down of guess left from 13
 	var tries = document.getElementById("life").innerHTML - 1;
-	var hidWord = document.getElementById("word").innerHTML;
-	var char = hidWord.search(/guess/g)
-	document.getElementById("dialog").innerHTML = char;
+	// loops through the target word
+	for (var i = 0; i < target.length; i++){
+		if (target.charAt(i) == guess){
+		indices.push(i);
+
+		}
+
+	}
+	// writes to the html
 	document.getElementById("log").innerHTML = guessed;	
 	document.getElementById("life").innerHTML = tries;
+		console.log(indices);
+		console.log(target);
+		console.log(target.length);
+		console.log(guess);
 }
-
