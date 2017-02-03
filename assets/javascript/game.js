de72dff2323721a69ebb518ebaa881cd9eb400f7
Benjamin Spille <Benjamin Spille>
2017-02-03 13:18:50 -0500

    game main components assembled

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,106 @@
-hgn = {
-	idxs: [],
-	wdBnk: ["Strike", "Freedom", "Justice", "Dominion"],
-	gssd: [],
-	tgt: "",
+hangman = {
+	cntDwn: [],
+	cntup: [0],
+	correct: "",
+	gessd: "",
+	trgt: "",
 	display: "",
+	wdBnk: ["Strike", "Freedom", "Justice", "Dominion"],
+	gallows: ["gallows8.gif", "gallows7.gif", "gallows6.gif", "gallows5.gif", "gallows4.gif", "gallows3.gif", "gallows2.gif", "gallows1.gif"],
 
 	wordGen: function()	{
 			// random generation of a word frome a word bank and stores is in tgt
-			this.tgt = hgn.wdBnk[Math.floor(Math.random() * hgn.wdBnk.length)];
+			this.trgt = this.wdBnk[Math.floor(Math.random() * this.wdBnk.length)];
 			// checks for all letters and generates a array record
-			var re = new RegExp(/[a-z A-Z]/, "g");
+			var re = new RegExp(/[a-z]/, "ig");
 			// replaces all characters in the tgt with _
-			this.display = this.tgt.replace(re, "_");
+			this.display = this.trgt.replace(re, "_");
+			this.cntDwn = 7;
+			this.gessd = "";
+			this.correct = "";
+			
 			// writes the initial display
+			document.getElementById("log").innerHTML = this.gessd;
 			document.getElementById("word").innerHTML = this.display;
+			document.getElementById("life").innerHTML = this.cntDwn;
+			document.getElementById("counter").innerHTML = this.cntup;
+			document.getElementById("mssg").innerHTML = "click play to start a new round";
+			document.getElementById("gImage").src = "assets/images/" + this.gallows[7];
 	},
 
-	srchTgt: function(k){
-		var re = new RegExp(k, "g");
-		var chk = this.tgt.search(re);
-		this.idxs.push(chk);
-	}
-	// need fun text
-	// need a picture changer
-	// need a turn counter
-	// need a function to replace underscores
-	// need a function to advance the game
-};
-document.getElementById("mssg").innerHTML = "hello";
-
-// document.getElementById("gImage").innerHTML = ;
-
-document.getElementById("dialog").innerHTML = "What is this?";
-
-document.getElementById("counter").innerHTML = 0;
-
-document.getElementById("life").innerHTML = 13;
-
-hgn.wordGen();
-
-
-document.onkeyup = function(event) {
- 	var ek = event.key;
- 	var kc = event.keyCode;
- 	// call test function from object and pass event key as a parameter
- 	hgn.test(event.key);
-
- 	// logged 65 - 90 for alpha keycodes
-
- 	for (i = -1; i < hgn.gssd.length; i++) {
- 		if (hgn.gssd.indexOf(ek) != -1) {
- 			break;
- 		}			
-		else { hgn.gssd.push(ek);
-			// loops through the target word | try to build a regular expression to perform this task
-			for (var i = 0; i < hgn.tgt.length; i++) {
-				if (hgn.tgt.charAt(i) === ek) {
-					hgn.idxs.push(i);
+	srchTrgt: function(k) {
+		console.log(k);
+		// passes the event.key as a parameter k to the regexp \event.key value\g
+		var re = new RegExp(k, "ig");
+		// do this function while condition is true
+		console.log("test_1" + this.cntDwn)
+		if (this.cntDwn > 0) {
+			// no repeat condition
+			var noRpt = this.gessd.match(re);
+			console.log(noRpt);
+			if ( noRpt == null) {
+				this.gessd = this.gessd + k;
+				console.log(this.gessd);
+				// writes the guesses to a log area on the html
+				document.getElementById("log").innerHTML = this.gessd;
+				// applies the regexp to the tgt string
+			
+				var find = this.trgt.match(re);
+				console.log(find);
+				//filters out null outputs
+				if (find != null) {
+					this.correct = this.correct + k;
+					console.log(this.correct);
+				}
+				else { // decrements the counter of turns for a failed guess
+					this.cntDwn--;
+					document.getElementById("gImage").src = "assets/images/" + this.gallows[this.cntDwn];
+					console.log(this.cntDwn);
+					document.getElementById("life").innerHTML = this.cntDwn;
 				}
 			}
 		}
+	},
+
+	compare: function() {
+		// creates a regexp  var that excludes characters form the crt array
+		var c = "[^" + this.correct + "]"
+		// constructs the regexp with runtime that results \[^the characters from the crt array]\g;
+		var re = new RegExp(c, "ig");
+		// rewrites the display string to only replace characters that have not been found
+		this.display = this.trgt.replace(re, "_");
+		// rewrites to the html
+		document.getElementById("word").innerHTML = this.display;
 	}
+
+	// need fun text
+	// need a picture changer
+};
+hangman.wordGen()
+$("#play").on("click", function() {
+	hangman.wordGen()
+});
+
+
+
+
+document.onkeyup = function(event) {
+	if ((hangman.cntDwn > 0) || (hangman.display != hangman.trgt)) {
+ 		var ek = event.key;
+ 		var kc = event.keyCode;
+ 		console.log(event.key);
+ 		// call test function from object and pass event key as a parameter
+ 		hangman.srchTrgt(event.key);
+ 		hangman.compare();
+ 		if (hangman.display == hangman.trgt){
+ 			hangman.cntup++;
+ 			document.getElementById("counter").innerHTML = hangman.cntup;
+ 			document.getElementById("mssg").innerHTML = "YOU WIN!";
+ 		}
+
+ 		if (hangman.cntDwn == 0) {
+ 			document.getElementById("mssg").innerHTML = "YOU LOSE!";
+ 		}
+ 	}
+ 	// logged 65 - 90 for alpha keycodes
 }
